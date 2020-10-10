82f7f26657ebc5d450b90d2b2ad77aefff3f5037
Kurt Guardia <kurtguardia@gmail.com>
2020-10-10 13:29:30 +0200

    created the Navbar and rendering conditions un App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import People from './components/People';
+import Planets from './components/Planets';
 
 function App() {
-  return <div className='App'></div>;
+  const [page, setPage] = useState('planets');
+
+  return (
+    <div className='App'>
+      <h1>Star Wars Info</h1>
+      <Navbar />
+      <div className='content'>{page ? <Planets /> : <People />}</div>
+    </div>
+  );
 }
 
 export default App;
